893062fe0b254957b377e40175020bd527dc0352
Zayd <166023703+povzayd@users.noreply.github.com>
2025-03-23 22:18:37 +0530

    v1

---
+++ s3gen.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#takes input a subdomain file
+read -p -e "Enter the path to subdomain file: " sub_file
+#check if the file exists
+if [ ! -f "${sub_file}" ]; then
+  echo "ERROR 404! ${sub_file} no such file."
+  exit 1
+fi
+#ENTER THE KEYWORD
+read -p -e "Enter the KEYWORD: " keyword
+#create output file
+output_file="${keyword}.txt"
+while read -r line; do
+  printf "%s-%s\n" "${keyword}" "${line}" >> "${output_file}"
+  printf "%s_%s\n" "${keyword}" "${line}" >> "${output_file}"
+  printf "%s-%s\n" "${line}" "${keyword}" >> "${output_file}"
+  printf "%s_%s\n" "${line}" "${keyword}" >> "${output_file}"
+done < "${sub_file}"
+echo "OUTPUT WRITTEN TO ${output_file}"
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
